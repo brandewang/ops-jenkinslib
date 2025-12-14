d8cc0fe04d09465e389610c4ffefb0bda0c9a8f3
ops <ops@ciicsh.com>
2025-12-14 07:23:23 +0000

    Update 2 files
    - /src/org/devops/Build.groovy
    - /Jenkinsfile

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,15 +1,24 @@
 @Library("mylib@main") _
-import org.devops.checkout
+import org.devops.*
 
 def checkout = new Checkout()
+def build = new Build()
 
 pipeline {
     agent { lable "build" }
     stages {
-        stage("CHeckout"){
+        stage("Checkout"){
             steps {
                 script {
                     checkout.GetCode("${env.srcUrl}", "${env.branchName}")
+                }
+            }
+        }
+
+        stage("Build"){
+            steps {
+                script {
+                    build.CodeBuild("maven")
                 }
             }
         }
---
+++ src/org/devops/Build.groovy
@@ -0,0 +1,41 @@
+package org.devops
+
+//Maven
+def MavenBuild(settingsPath="./settings.xml"){
+    sh "mvn clean package -DskipTests -s ${settingsPath}"
+}
+
+//Gradle
+def GradleBUild(){
+    sh "gradle build -x test"
+}
+
+//Golang
+def GoBuild(){
+    sh "go build demo.go"
+}
+
+//Npm
+def NpmBuild(){
+    sh "npm install && npm run build"
+}
+
+//Yarm
+def YarnBuild(){
+    sh "yarn install && yarn build"
+}
+
+//Main
+def CodeBuild(type, configPath=""){
+    switch(type){
+        case "maven":
+            MavenBuild(settingsPath="./settings.xml")
+            break;
+        case "gradle"
+            GradleBUild()
+            break;
+        default:
+            error "No such tools ... [maven/ant/gradle/npm/yarm/go]"
+            break;
+    }
+}
