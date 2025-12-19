723e105425465e4c339e2215704f5eb8bb42a66f
王于晟 <brande.wang@hotmail.com>
2025-12-19 13:17:00 +0800

    1

---
+++ src/org/devops/Sonar.groovy
@@ -0,0 +1,40 @@
+package org.devops
+
+def SonarJava(projectName, groupName){
+    withCredentials([string(credentialsId: '', variable: 'AUTH_TOKEN')]){
+        sh """
+            sonar-scanner -Dsonar.host.url=http://192.168.0.1:9000 \
+                -Dsonar.projectKey=${projectName} \
+                -Dsonar.projectName=${projectName} \
+                -Dsonar.projectVersion=${BUILD_ID} \
+                -Dsonar.login=${AUTH_TOKEN} \
+                -Dsonar.ws.timeout=30 \
+                -Dsonar.projectDescription="my first project!" \
+                -Dsonar.links.homepage="" \
+                -Dsonar.links.ci="" \
+                -Dsonar.sources=src \
+                -Dsonar.sourceEncoding=UTF-8 \
+                -Dsonar.java.binaries=target/classes \
+                -Dsonar.java.test.binaries=target/test-classes \
+                -Dsonar.java.surefire.report=target/surefire-reports
+        """
+    }
+}
+
+def SonarJava(projectName, groupName){
+    withCredentials([string(credentialsId: '', variable: 'AUTH_TOKEN')]){
+        sh """
+            sonar-scanner -Dsonar.host.url=http://192.168.0.1:9000 \
+                -Dsonar.projectKey=${projectName} \
+                -Dsonar.projectName=${projectName} \
+                -Dsonar.projectVersion=${BUILD_ID} \
+                -Dsonar.login=${AUTH_TOKEN} \
+                -Dsonar.ws.timeout=30 \
+                -Dsonar.projectDescription="my first project!" \
+                -Dsonar.links.homepage="" \
+                -Dsonar.links.ci="" \
+                -Dsonar.sources=src \
+                -Dsonar.sourceEncoding=UTF-8
+        """
+    }
+}
