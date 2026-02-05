b3aa688893880be4693bf1cd518d6e4f0f6e0be1
王于晟 <wangysh@ciicsh.com>
2026-02-05 11:33:07 +0800

    sonarqube

--- src/org/devops/Sonar.groovy
+++ src/org/devops/Sonar.groovy
@@ -1,40 +1,52 @@
 package org.devops
 
-def SonarJava(projectName, groupName){
-    withCredentials([string(credentialsId: '', variable: 'AUTH_TOKEN')]){
+def SonarJava(projectName, projectDesc, projectVersion){
+    def sonarServer = 'http://192.168.0.1:9000'
+    withCredentials([string(credentialsId: '702d71be-1238-474f-9755-18728aac9c81', variable: 'AUTH_TOKEN')]){
         sh """
-            sonar-scanner -Dsonar.host.url=http://192.168.0.1:9000 \
-                -Dsonar.projectKey=${projectName} \
-                -Dsonar.projectName=${projectName} \
-                -Dsonar.projectVersion=${BUILD_ID} \
-                -Dsonar.login=${AUTH_TOKEN} \
-                -Dsonar.ws.timeout=30 \
-                -Dsonar.projectDescription="my first project!" \
-                -Dsonar.links.homepage="" \
-                -Dsonar.links.ci="" \
-                -Dsonar.sources=src \
-                -Dsonar.sourceEncoding=UTF-8 \
-                -Dsonar.java.binaries=target/classes \
-                -Dsonar.java.test.binaries=target/test-classes \
-                -Dsonar.java.surefire.report=target/surefire-reports
+            sonar-scanner -Dsonar.host.url=${sonarServer} \
+            -Dsonar.projectKey=${projectName} \
+            -Dsonar.projectName=${projectName} \
+            -Dsonar.projectVersion=${projectVersion} \
+            -Dsonar.token=sqa_30d3307f2699199ecca914f87d2c2e240c59b7f0 \
+            -Dsonar.ws.timeout=30 \
+            -Dsonar.projectDescription=${projectDesc} \
+            -Dsonar.links.homepage=http://www.baidu.com \
+            -Dsonar.sources=src \
+            -Dsonar.sourceEncoding=UTF-8 \
+            -Dsonar.java.binaries=target/classes \
+            -Dsonar.java.test.binaries=target/test-classes \
+            -Dsonar.java.surefire.report=target/surefire-reports
         """
     }
 }
 
-def Sonar(projectName, groupName){
-    withCredentials([string(credentialsId: '', variable: 'AUTH_TOKEN')]){
+def SonarOther(projectName, projectDesc, projectVersion){
+    def sonarServer = 'http://192.168.0.1:9000'
+    withCredentials([string(credentialsId: '702d71be-1238-474f-9755-18728aac9c81', variable: 'AUTH_TOKEN')]){
         sh """
-            sonar-scanner -Dsonar.host.url=http://192.168.0.1:9000 \
-                -Dsonar.projectKey=${projectName} \
-                -Dsonar.projectName=${projectName} \
-                -Dsonar.projectVersion=${BUILD_ID} \
-                -Dsonar.login=${AUTH_TOKEN} \
-                -Dsonar.ws.timeout=30 \
-                -Dsonar.projectDescription="my first project!" \
-                -Dsonar.links.homepage="" \
-                -Dsonar.links.ci="" \
-                -Dsonar.sources=src \
-                -Dsonar.sourceEncoding=UTF-8
+            sonar-scanner -Dsonar.host.url=${sonarServer} \
+            -Dsonar.projectKey=${projectName} \
+            -Dsonar.projectName=${projectName} \
+            -Dsonar.projectVersion=${projectVersion} \
+            -Dsonar.token=sqa_30d3307f2699199ecca914f87d2c2e240c59b7f0 \
+            -Dsonar.ws.timeout=30 \
+            -Dsonar.projectDescription=${projectDesc} \
+            -Dsonar.links.homepage=http://www.baidu.com \
+            -Dsonar.sources=src \
+            -Dsonar.sourceEncoding=UTF-8 
         """
+    }
+}
+
+//Main
+def SonarScan(type, prjectName, projectDesc, projectVersion){
+    switch(type){
+        case "maven":
+            SonarJava(prjectName, projectDesc, projectVersion)
+            break;
+        default:
+            SonarOther(prjectName, projectDesc, projectVersion)
+            break;
     }
 }
