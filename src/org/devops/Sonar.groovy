f5513d8218c6f4edd5d5e6fa20d35edaca0cbfe2
王于晟 <wangysh@ciicsh.com>
2026-02-06 12:35:39 +0800

    1

--- src/org/devops/Sonar.groovy
+++ src/org/devops/Sonar.groovy
@@ -19,17 +19,14 @@ def SonarJava(projectName, projectDesc, projectVersion, homePage){
 }
 
 def SonarOther(projectName, projectDesc, projectVersion){
-    def sonarServer = 'http://192.168.5.80:9000'
-    withCredentials([string(credentialsId: '702d71be-1238-474f-9755-18728aac9c81', variable: 'AUTH_TOKEN')]){
+    withSonarQubeEnv("sonarqube-server"){
         sh """
-            sonar-scanner -Dsonar.host.url=${sonarServer} \
-            -Dsonar.projectKey=${projectName} \
+            sonar-scanner -Dsonar.projectKey=${projectName} \
             -Dsonar.projectName=${projectName} \
             -Dsonar.projectVersion=${projectVersion} \
-            -Dsonar.token=sqa_30d3307f2699199ecca914f87d2c2e240c59b7f0 \
             -Dsonar.ws.timeout=30 \
             -Dsonar.projectDescription=${projectDesc} \
-            -Dsonar.links.homepage=http://www.baidu.com \
+            -Dsonar.links.homepage=${homePage} \
             -Dsonar.sources=src \
             -Dsonar.sourceEncoding=UTF-8 
         """
@@ -40,10 +37,10 @@ def SonarOther(projectName, projectDesc, projectVersion){
 def SonarScan(type, prjectName, projectDesc, projectVersion){
     switch(type){
         case "maven":
-            SonarJava(prjectName, projectDesc, projectVersion)
+            SonarJava(prjectName, projectDesc, projectVersion, homePage)
             break;
         default:
-            SonarOther(prjectName, projectDesc, projectVersion)
+            SonarOther(prjectName, projectDesc, projectVersion, homePage)
             break;
     }
 }
